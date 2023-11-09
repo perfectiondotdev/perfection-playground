3cc6b8a2ebe33e1a9d21a07366d9c045d705f474
Ugo <54071701+ugo-quaisse@users.noreply.github.com>
2023-11-09 14:32:26 +0100

    Add Perfection Theme

--- examples/nextjs-app/app/_components/atoms/card.js
+++ examples/nextjs-app/app/_components/atoms/card.js
@@ -1,7 +1,7 @@
 export default async function Card() {
   return (
     <>
-      <div data-pf='{"componentKey": "gridCard", "title": "Card", "groupId": "gridCard"}'>Card component</div>
+      <div data-pf='{"componentKey": "card", "title": "Card", "groupId": "gridCard"}'>Card component</div>
     </>
   );
 }
---
+++ perfection-theme/README.md
@@ -0,0 +1,3 @@
+# Perfection Theme
+
+To create and manage your Theme, please read that first https://docs.perfection.dev/general/quickstart/create-theme
