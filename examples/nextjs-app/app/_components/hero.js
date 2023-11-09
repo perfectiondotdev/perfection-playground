481afb57973abdd413046ca9961189ab3e8cf4a1
Ugo <54071701+ugo-quaisse@users.noreply.github.com>
2023-11-09 16:37:19 +0100

    Fix links

--- examples/nextjs-app/app/_components/hero.js
+++ examples/nextjs-app/app/_components/hero.js
@@ -6,14 +6,14 @@ export default function Hero() {
   return (
     <header className="ui-header">
       {" "}
-      <Link href="/" className="ui-button-link">
+      <Link href="https://github.com/perfectiondotdev/perfection/tree/main/examples" target="_blank" className="ui-button-link">
         <GitHub />
         Discover more examples
         <Chevron />
       </Link>
       <h1>Supercharge your preview environment</h1>
       <h2>Streamline your editing process and increase your team productivity to deliver engaging digital experiences faster.</h2>
-      <Link href="/" className="ui-cta">
+      <Link href="https://docs.perfection.dev/" target="_blank" className="ui-cta">
         Read the doc
       </Link>
     </header>
--- examples/nextjs-app/app/_components/svgs/chevron.js
+++ examples/nextjs-app/app/_components/svgs/chevron.js
@@ -3,8 +3,8 @@ export const Chevron = () => (
     <g id="heroicons-solid/chevron-right">
       <path
         id="Vector 335 (Stroke)"
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M13.5669 9.55806C13.811 9.80214 13.811 10.1979 13.5669 10.4419L7.31694 16.6919C7.07286 16.936 6.67714 16.936 6.43306 16.6919C6.18898 16.4479 6.18898 16.0521 6.43306 15.8081L12.2411 10L6.43306 4.19194C6.18898 3.94786 6.18898 3.55214 6.43306 3.30806C6.67714 3.06398 7.07286 3.06398 7.31694 3.30806L13.5669 9.55806Z"
         fill="white"
       />
