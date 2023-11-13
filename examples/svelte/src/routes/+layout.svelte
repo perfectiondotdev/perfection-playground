dd743131df6b7c30bc76a8c9d20e5fc331442497
Ugo <54071701+ugo-quaisse@users.noreply.github.com>
2023-11-13 22:41:22 +0100

    Update layout.svelte

--- examples/svelte/src/routes/+layout.svelte
+++ examples/svelte/src/routes/+layout.svelte
@@ -3,20 +3,5 @@
 	import '../app.css';
 </script>
 
-<html lang="en">
-	<head>
-		<meta charset="UTF-8" />
-		<meta name="description" content="Perfection Example for Svelte" />
-		<meta name="viewport" content="width=device-width" />
-		<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-		<meta name="generator" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-		<title>Perfection</title>
-	</head>
-	<body
-		data-pf={`data-pf='{"pageId": "homepage", "providerId":"generic-provider","entryId": "homepage"}'`}
-	>
-		<Header />
-		<slot />
-	</body>
-</html>
+<Header />
+<slot />
